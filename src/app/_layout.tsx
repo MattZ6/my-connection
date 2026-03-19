3ac8dd9a635c4eedfa7c2716b10521b9f520537c
Matheus Felipe Zanin <matt_z6@hotmail.com>
2026-03-19 15:28:50 -0300

    chore(root-layout): improve styles

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,40 +4,58 @@ import {
   Poppins_700Bold,
   useFonts,
 } from "@expo-google-fonts/poppins";
-import { Stack } from "expo-router";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Color, Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { theme } from "@/theme";
 
 SplashScreen.preventAutoHideAsync();
 
-export default function MainLayout() {
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_700Bold,
   });
 
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
     return null;
   }
 
-  SplashScreen.hideAsync();
-
   return (
     <SafeAreaProvider>
-      <StatusBar style="light" />
-
-      <Stack
-        screenOptions={{
-          freezeOnBlur: true,
-          headerShown: false,
-          contentStyle: {
-            backgroundColor: theme.colors.mauve1,
-          },
-        }}
-      />
+      <StatusBar animated style="auto" />
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack
+          screenOptions={{
+            contentStyle: {
+              backgroundColor: Color.android.dynamic.surface,
+            },
+            headerStyle: {
+              backgroundColor: Color.android.dynamic.surface.toString(),
+            },
+            headerShadowVisible: false,
+            headerTitleStyle: {
+              color: Color.android.dynamic.onSurfaceInverse.toString(),
+            },
+          }}
+        />
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
