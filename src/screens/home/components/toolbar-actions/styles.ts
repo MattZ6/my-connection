70d5f49efbcd089ae706d9f4436c2fd67320b895
Matheus Felipe Zanin <matt_z6@hotmail.com>
2026-04-15 10:29:10 -0300

    fix(refresh-button): styles for android

--- src/screens/home/components/toolbar-actions/styles.ts
+++ src/screens/home/components/toolbar-actions/styles.ts
@@ -3,23 +3,25 @@ import { StyleSheet } from "react-native";
 import type { ThemeConfig } from "@/theme/types";
 
 export function getStyles(theme: ThemeConfig) {
-  const size = 56;
+  const size = 48;
 
   return StyleSheet.create({
     androidButtonContainer: {
       alignItems: "center",
       justifyContent: "center",
       padding: 8,
-      marginRight: -16,
+      marginRight: -12,
       width: size,
       height: size,
       borderRadius: size,
       overflow: "hidden",
     },
     androidButton: {
-      flex: 1,
       alignItems: "center",
       justifyContent: "center",
+      width: size,
+      height: size,
+      borderRadius: size,
     },
     icon: {
       color: theme.colors.content.base,
