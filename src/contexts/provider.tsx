7072c7d96d23a03d4898f978a667853f65231aec
Matheus Zanin <matt_z6@hotmail.com>
2026-04-29 17:02:29 -0300

    refactor(provider): move providers to a single file (#78)

---
+++ src/contexts/provider.tsx
@@ -0,0 +1,55 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider as NavigationThemeProvider,
+} from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import type { ReactNode } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { useTheme } from "@/hooks/use-theme";
+
+import { LocationProvider } from "./location";
+import { NetworkUpdatesProvider } from "./network-updates";
+import { PreferencesProvider } from "./preferences";
+import { ThemeProvider } from "./theme";
+
+type NavigationProviderProps = {
+  children: ReactNode;
+};
+
+function NavigationProvider({ children }: NavigationProviderProps) {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <NavigationThemeProvider
+      value={resolvedTheme === "dark" ? DarkTheme : DefaultTheme}
+    >
+      <StatusBar animated style={resolvedTheme === "dark" ? "light" : "dark"} />
+      {children}
+    </NavigationThemeProvider>
+  );
+}
+
+type Props = {
+  children: ReactNode;
+};
+
+export function Provider(props: Props) {
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <PreferencesProvider defaultHapticsEnabled={true}>
+          <LocationProvider>
+            <NetworkUpdatesProvider
+              defaultAutomaticUpdatesEnabled={true}
+              defaultUpdateFrequency="60s"
+            >
+              <NavigationProvider {...props} />
+            </NetworkUpdatesProvider>
+          </LocationProvider>
+        </PreferencesProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
+}
